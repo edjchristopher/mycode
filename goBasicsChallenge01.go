56a32ee25a2943b2ce848ac972d3781c20896ce8
edjchristopher@gmail.com <edjchristopher@gmail.com>
2022-10-31 16:14:21 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,11 @@
+/* Alta3 Research | EdC
+   Variables - Package and function level   */
+
+package main
+  
+import "fmt"
+
+func main() {
+    fmt.Println("Hello EdC")
+    fmt.Println("Syracuse, New York")
+}            
