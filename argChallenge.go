fa74979baa710ec0690f50641df857e557912ccb
edjchristopher@gmail.com <edjchristopher@gmail.com>
2022-11-02 15:14:11 +0000

    fun with flags

---
+++ argChallenge.go
@@ -0,0 +1,37 @@
+/* Alta3 Research | RZFeeser
+   Reading positional arguments from the command line*/
+   
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+
+    // os.Args provides access to raw CLI arguments
+    argsWithProg := os.Args         // includes program name
+    
+    // argsWithoutProg := os.Args[1:]  // does not include program name
+
+    // arg := os.Args[3]               // select the 3rd argument
+                                    // remember: the 1st argument is the name
+                                    //           of the program
+
+    // last_arg := os.Args[len(os.Args)-1]         // select the last argument         // select the last argument
+
+    // display the results
+    fmt.Println("s is the number of ARGS passed:")
+    fmt.Println(len(argsWithProg))
+ 
+
+
+    
+
+
+    
+
+
+}
+
